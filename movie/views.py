2739e8733b8949a0ea4914aab4d75b38ae6876e1
NoprauxX12 <juanselizca@outlook.com>
2024-02-08 10:55:34 -0500

    Cambios en la función main

--- movie/views.py
+++ movie/views.py
@@ -6,9 +6,9 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1> Welcome to Home Page <h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Juan Sebastian'})
+    return render(request, 'home.html', {'name': 'Juan Sebastian Lizcano Urrea'})
 
 def about(request):
     #return HttpResponse('<h1> Welcome to About Page <h1>')
-    return render(request, 'about.html', {'name': 'Juan Sebastian'})
+    return render(request, 'about.html', {'name': 'Juan Sebastian Lizcano Urrea'})
     
